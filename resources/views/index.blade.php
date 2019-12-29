c89cfc3f49c258d8d3c4f8813d2e5379ff577e2c
adanolivos <adanolivos@gmail.com>
2019-12-28 19:45:28 -0500

    change layout

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,81 +1,109 @@
 @extends('master.layout')
 @section('title', 'Adan Olivos')
 @section('content')
-<div class="main-section">
-    <div class="content">
-        <h1 class="top-left">Adan Olivos</h1>
-        <div class="bottom-right">
-            <h2>Computer Science Graduate</h2>
-            <p>Interest in mobile and web developement.</p>
-        </div>
+<div class="container-fluid">
+    <div class="jumbotron">
+        <img class="profile-img" src="{{asset('img/download.jpeg')}}">
+        <h1>Adan Olivos</h1>    
+        <h2>Senior Systems Engineer</h2>
+        <p>IAM enthusiast</p>
+        <p>Interest in mobile and web developement.</p>
     </div>
 </div>
-<!-- About -->
-<div class="section bg-white">
-    <div class="section-content">
-        <div class="title">
-            <h3>About</h3>
-            <br>
-            <span><i class="far fa-user fa-5x"></i></span>
-        </div>
-        <div class="content">
-            <h3>Adan Olivos</h3>
-            <p>
-                As a recent Computer Graduate from New Jersey City University, I bring to the table three years in customer
-                service and three years in freelance in web development.
-            </p>
-            <br>
-            <p>
-                Offering creative solutions on web development using skills in HTML, CSS, JavaScript, and PHP, with
-                plenty understanding using databases such as SQL and Mysql. I also understand the basics of Search
-                Optimization Engines and marketing strategies.
-            </p>
-            <br>
-            <p> As a web developer, I understand Agile development process, and write code accordingly to industry standards,
-                as well as code reusability.
-            </p>
-        </div>
+<!-- about -->
+<div class="container-fluid bg-white">
+    <div class="row">
+        <h2>About me</h2>
+        <div class="clearfix"></div>
+        <p class="paragraph">
+            As a Senior Systems Engineer, I'll bring to the table a year plus of experience in Access Management, Directory Services, &amp; Identity Management, 
+            also bring three years of freelance web development experience and seven years of customer service.
+        </br>
+            I have work and understand Agile and Waterfall developement methodologies, and work well as part of a team as well as individually.
+        </p>
+        <table class="table table-striped">
+            <thead class="thead-dark">
+               <tr>
+                   <th colspan="2">Development tools experience</th>
+               </tr>
+            </thead>
+            <tbody class="table-body">
+                <tr>
+                    <td>Operating Systems</td>
+                    <td>Windows, MacOs, Linux</td>  
+                </tr>
+                <tr>
+                    <td>Producivity Tools</td>
+                    <td>Jira, Trello, Microsoft suite, Google suite</td>
+                </tr>
+                <tr>
+                    <td>Database Systems</td>
+                    <td>MongoDB, MySql</td>
+                </tr>
+                <tr>
+                    <td>Version Control</td>
+                    <td>Bitbucket, Github</td>
+                </tr>
+                <tr>
+                    <td>Programming Languages</td>
+                    <td>Java, Python, Javascript, PHP</td>
+                </tr>
+                <tr>
+                    <td>Frameworks and Processors</td>
+                    <td>Bootstrap, Laravel, Sass</td>
+                </tr>
+                <tr>
+                    <td>Miscellaneous</td>
+                    <td>Ansible, bash scripting, Wordpress</td>
+                </tr>
+                <tr>
+                    <td>IAM</td>
+                    <td>Forgerock Access Management, Identity Management, Directory Services</td>
+                </tr>
+            </tbody>
+        </table>
     </div>
 
 </div>
-
 <!-- Education -->
-<div class="section bg-blue">
-    <div class="section-content">
-        <div class="title">
-            <h3>Education</h3>
-            <br>
-            <span><i class="fa fa-graduation-cap fa-5x"></i></span>
-        </div>
-        <div class="content">
-            <div class="box">
-                <h3>New Jersey City University, Jersey City.</h3>
-                <p><strong>Bachelors Computer Science</strong></p>
-                <p>G.P.A: 3.2</p>
-                <p>Graduation: December 2018.</p>
+<div class="container-fluid bg-white">
+    <div class="row">
+    <h2>Education</h2>
+    <div class="clearfix"></div>
+        <!--<h3>Education</h3>-->
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-body">
+                    <div class="card-title"> NJCU</div>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
             </div>
-            <div class="box">
-                <h3>Hudson County Community College, Jersey City.</h3>
-                <p><strong>Associates in Computer Science</strong></p>
-                <p>G.P.A: 3.0</p>
-                <p>Graduation: May 2011.</p>
+        </div>
+        <div class="col-md-12">
+            <div class="card">
+            <div class="card-body">
+                    <div class="card-title"> NJCU</div>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
             </div>
         </div>
     </div>
 </div>
 
 <!-- Experience -->
-<div class="section bg-white">
+<div class="container-fluid bg-white">
+    <h2>Experience</h2>
     <div class="section-content">
-        <div class="title">
-            <h3>Experience</h3>
-            <br>
-            <span><i class="fa fa-user-tie fa-5x"></i></span>
-        </div>
-        <div class="content">
+        <div class="ex-content">
+        <div class="ex-box">
+                <h3>Hub City Media</h3>
+                <em class="location">South Plainfiel, NJ.</em>
+                <em class="date">08/2019 - Present</em>
+                <p>Web Development</p>
+            </div>
             <div class="ex-box">
                 <h3>Freelance</h3>
-                <em class="date">05/2015 - Present</em>
+                <em class="date">05/2015 - 08/2018</em>
                 <p>Web Development</p>
             </div>
             <div class="ex-box">
@@ -95,136 +123,133 @@
 </div>
 
 <!-- Freelance -->
-<div class="section bg-blue">
+<div class="container-fluid bg-white">
     <div class="section-content">
-        <div class="title">
-            <h3>Freelance Work</h3>
-            <br>
-            <span><i class="fa fa-briefcase fa-5x"></i></span>
-        </div>
-        <div class="content">
-            <div class="panel">
-                <div class="img-box">
-                    <div class="clearfix">
-                        <div class="image-container">
-                            <img src="{{asset('img/PassaicVisionCenter.png')}}">
-                        </div>
+        <h2>Freelance</h2>
+        <div class="row">
+            <div class="col-md-6">
+                <div class="card">
+                    <img class="card-img-top" src="{{asset('img/PassaicVisionCenter.png')}}">
+                    <div class="card-body">
+                        <div class="card-title">Passaic Vision Center</div>
+                        <p>Freelance project for an Optometrist. This project help him be known
+                        by the Passaic County Community. It implemented S.E.O strategy, Agile development
+                            process, and budget planning. <a href="http://gentle-retreat-33476.herokuapp.com/" target="_blank"><i class="fa fa-share-square"></i></a> 
+                        </p>
                     </div>
-                    <h3>Passaic Vision Center</h3>
-                    <hr>
-                    <p>Freelance project for an Optometrist. This project help him be known
-                    by the Passaic County Community. It implemented S.E.O strategy, Agile development
-                        process, and budget planning. <a href="http://gentle-retreat-33476.herokuapp.com/" target="_blank"><i class="fa fa-share-square"></i></a> </p>
-
                 </div>
             </div>
-            <div class="panel">
-                <div class="img-box">
-                    <div class="clearfix">
-                        <div class="image-container">
-                            <img src="{{asset('img/CliftonEyeSpecialists.png')}}">
-                        </div>
+            <div class="col-md-6">
+                <div class="card">
+                    <img class="card-img-top" src="{{asset('img/CliftonEyeSpecialists.png')}}">
+                    <div class="card-body">
+                        <div class="card-title">Clifton Eye Specialists</div>
+                        <p>Freelance project for an Optometrist. This project help him be known
+                            by the Passaic County Community. It implemented S.E.O strategy, Agile development
+                            process, and budget planning. <a href="http://warm-ridge-65957.herokuapp.com" target="_blank"><i class="fa fa-share-square"></i></a> 
+                        </p>
                     </div>
-                    <h3>Clifton Eye Specialists</h3>
-                    <hr>
-                    <p>Freelance project for an Optometrist. This project help him be known
-                        by the Passaic County Community. It implemented S.E.O strategy, Agile development
-                        process, and budget planning. <a href="http://warm-ridge-65957.herokuapp.com" target="_blank"><i class="fa fa-share-square"></i></a> </p>
-
                 </div>
             </div>
         </div>
     </div>
 </div>
+
 <!-- projects -->
-<div class="section bg-white">
+<div class="container-fluid bg-white">
     <div class="section-content">
-        <div class="title">
-            <h3>projects</h3>
-            <br>
-            <span><i class="fas fa-project-diagram fa-5x"></i></span>
-        </div>
-        <div class="content">
-            <div class="panel">
-                <div class="img-box">
-                    <div class="clearfix">
-                        <div class="image-container">
-                            <img src="{{asset('img/GetFit.png')}}">
-                        </div>
+        <h2>Projects</h2>
+        <div class="row">
+            <div class="col-md-6">
+                <div class="card">
+                    <img class="card-img-top" src="{{asset('img/GetFit.png')}}">
+                    <div class="card-body">
+                        <div class="title"> Get Fit</div>
+                        <p>A personal project that will help an user to keep his or hers physical
+                            goals. <a href="http://polar-escarpment-87243.herokuapp.com/" target="_blank"><i class="fa fa-share-square"></i></a>
+                        </p>
                     </div>
-                    <h3>Get Fit</h3>
-                    <hr>
-                    <p>A personal project that will help an user to keep his or hers physical
-                        goals. <a href="http://polar-escarpment-87243.herokuapp.com/" target="_blank"><i class="fa fa-share-square"></i></a></p>
                 </div>
             </div>
-            <div class="panel">
-                <div class="img-box">
-                    <div class="clearfix">
-                        <div class="image-container">
-                            <img src="{{asset('img/MdDirectory.png')}}">
-                        </div>
+            <div class="col-md-6">
+                <div class="card">
+                    <img class="card-img-top" src="{{asset('img/MdDirectory.png')}}">
+                    <div class="card-body">
+                        <div class="title"> Md. Directory</div>
+                        <p>M.D. Directory is a personal project that helps users to seek for local medical help.
+                            also it helps doctors to be in touch with their patients.
+                            <a href="http://fast-oasis-24569.herokuapp.com/" target="_blank"><i class="fa fa-share-square"></i></a>
+                        </p>
                     </div>
-                    <h3>Md. Directory</h3>
-                    <hr>
-                    <p>M.D. Directory is a personal project that helps users to seek for local medical help.
-                        also it helps doctors to be in touch with their patients.
-                        <a href="http://fast-oasis-24569.herokuapp.com/" target="_blank"><i class="fa fa-share-square"></i></a></p>
                 </div>
             </div>
         </div>
     </div>
 </div>
 <!-- Skills -->
-<div class="section bg-blue">
+<div class="container-fluid bg-white">
     <div class="section-content">
-        <div class="title">
-            <h3>Skills</h3>
-            <br>
-            <span><i class="fa fa-toolbox fa-5x"></i></span>
-        </div>
-        <div class="content">
-            <div class="dual-col">
-                <h3>HTML/CSS</h3>
-                <div class="w3-progress-container">
-                    <div class="w3-progressbar" style="width:90%;"></div>
+        <h2>Skils</h2>
+        <div class="row">
+            <div class="col-md-3">
+                <h6>HTML/CSS</h6>
+                <div class="progress">
+                    <div class="progress-bar" role="progressbar" style="width: 70%" aria-valuenow="70%" aria-valuemin="0" aria-valuemax="100"></div>
                 </div>
             </div>
-            <div class="dual-col">
-                <h3>JavaScript</h3>
-                <div class="w3-progress-container">
-                    <div class="w3-progressbar" style="width:70%;"></div>
+            <div class="col-md-3">
+                <h6>Javascript</h6>
+                <div class="progress">
+                    <div class="progress-bar" role="progressbar" style="width: 70%" aria-valuenow="70%" aria-valuemin="0" aria-valuemax="100"></div>
                 </div>
             </div>
-            <div class="dual-col">
-                <h3>MySql</h3>
-                <div class="w3-progress-container">
-                    <div class="w3-progressbar" style="width:85%;"></div>
+            <div class="col-md-3">
+                <h6>MySql</h6>
+                <div class="progress">
+                    <div class="progress-bar" role="pogressbar" style="width: 60%" aria-valuenow="60" area-valuemin="0" aria-valuemax="100"></div>
                 </div>
             </div>
-            <div class="dual-col">
-                <h3>GitHub</h3>
-                <div class="w3-progress-container">
-                    <div class="w3-progressbar" style="width:75%;"></div>
+            <div class="col-md-3">
+                <h6>Github</h6>
+                <div class="progress">
+                    <div class="progress-bar" role="progressbar" style="width: 75%" aria-valuenow="75%" aria-valuemin="0" aria-valuemax="100"></div>
                 </div>
             </div>
-            <div class="dual-col">
-                <h3>MS Suite</h3>
-                <div class="w3-progress-container">
-                    <div class="w3-progressbar" style="width:80%;"></div>
+            <div class="col-md-3">
+                <h6>MS Suite</h6>
+                <div class="progress">
+                    <div class="progress-bar" role="progressbar" style="width: 70%" aria-valuenow="70%" aria-valuemin="0" aria-valuemax="100"></div>
                 </div>
             </div>
-            <div class="dual-col">
-                <h3>PHP</h3>
-                <div class="w3-progress-container">
-                    <div class="w3-progressbar" style="width:75%;"></div>
+            <div class="col-md-3">
+                <h6>Python</h6>
+                <div class="progress">
+                    <div class="progress-bar" role="progressbar" style="width: 80%" aria-valuenow="80%" aria-valuemin="0" aria-valuemax="100"></div>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <h6>Java</h6>
+                <div class="progress">
+                    <div class="progress-bar" role="progressbar" style="width: 75%" aria-valuenow="75%" aria-valuemin="0" aria-valuemax="100"></div>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <h6>MongoDB</h6>
+                <div class="progress">
+                    <div class="progress-bar" role="progressbar" style="width: 70%" aria-valuenow="70%" aria-valuemin="0" aria-valuemax="100"></div>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <h6>PHP</h6>
+                <div class="progress">
+                    <div class="progress-bar" role="progressbar" style="width: 75%" aria-valuenow="70%" aria-valuemin="0" aria-valuemax="100"></div>
                 </div>
             </div>
         </div>
     </div>
 </div>
 
-<!-- Social -->
+<!-- Social 
 <div class="section">
     <div class="section-content">
         <div class="title">
@@ -242,4 +267,5 @@
         </div>
     </div>
 </div>
+-->
 @endsection
--- resources/views/master/layout.blade.php
+++ resources/views/master/layout.blade.php
@@ -3,7 +3,8 @@
 <head>
     <title>@yield('title')</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="{{URL :: asset('css/main.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{URL :: asset('css/bootstrap.min.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{URL :: asset('css/index.css')}}">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
     <link rel="stylesheet" media="screen and (min-width:320px) and (max-width:700px)" href="{{asset('css/mobile-main.css')}}">
 </head>
